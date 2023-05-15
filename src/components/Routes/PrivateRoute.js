053fdd8bbcc7e3c53d7f97dfb6d55ed999255c52
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-05-15 09:30:45 +0300

    fixed/page redirect from contacts

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -2,7 +2,8 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router';
 
 export const PrivateRoute = ({ children, ...routeProps }) => {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-  return isLoggedIn ? children : <Navigate to="/login" />;
+  const token = useSelector(state => state.auth.token);
+
+  return token ? children : <Navigate to="/login" />; 
 };
