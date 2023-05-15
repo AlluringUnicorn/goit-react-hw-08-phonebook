0f739e0538b13dd05bc3889cd503f604b2945f72
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-05-15 21:17:39 +0300

    Update PrivateRoute.js

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -4,7 +4,8 @@ import { Navigate } from 'react-router';
 export const PrivateRoute = ({ children, ...routeProps }) => {
 
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
-  console.log(isLoggedIn);
 
   return isLoggedIn ? children : <Navigate to="/login" /> ; 
 };
+
+
